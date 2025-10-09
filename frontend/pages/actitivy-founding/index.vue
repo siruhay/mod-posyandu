f43e9db75335f6ff93c1ee9cd63c5c14e831bb94
sudarmono <30992530+monoland@users.noreply.github.com>
2025-10-09 15:25:17 +0700

    update

---
+++ frontend/pages/actitivy-founding/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<page-crud
+		page-name="posyandu-founding"
+		page-key="founding"
+		parent-name="posyandu-actitivy"
+		parent-key="actitivy"
+	></page-crud>
+</template>
+
+<script>
+export default {
+	name: "posyandu-founding",
+};
+</script>
